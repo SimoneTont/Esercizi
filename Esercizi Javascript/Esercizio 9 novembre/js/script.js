37960c30b372283209b578db66f767df6a2e12ba
Simone Tonti <simone.tonti98@gmail.com>
2023-11-09 17:37:58 +0100

    9 novembre

---
+++ Esercizi Javascript/Esercizio 9 novembre/js/script.js
@@ -0,0 +1,51 @@
+console.log(window)
+//prompt che aggiunge paragrafo
+/*const newTask = function ()
+{
+    let a=prompt("Add new task")
+    if (a===null)
+    {}
+    else
+    {
+        let div=document.querySelector('div')
+        let pNew = document.createElement("p");
+        pNew.appendChild(document.createTextNode(a));
+        div.appendChild(pNew);
+    }
+}*/
+
+//Da campo input a pragrafo
+let i=0
+
+
+const submitTask = function ()
+{
+    //variabili
+    let input=document.getElementsByTagName("input")[0].value
+    let div=document.querySelector('article')
+    let pNew = document.createElement("p")
+    let deleteButton = document.createElement("button")
+    //testo
+    pNew.appendChild(document.createTextNode(input));
+    deleteButton.appendChild(document.createTextNode("Delete the task above"));
+    //aggiungi a pagina
+    div.appendChild(pNew);
+    div.appendChild(deleteButton);
+    //proprietà click
+    pNew.onclick =function() 
+    {
+        pNew.className ='Completo'
+    }
+    deleteButton.onclick =function() 
+    {
+        pNew.remove()
+        deleteButton.remove()
+    }
+}
+const deleteAll = function ()
+{
+    let AllDivP=document.querySelectorAll('article > p')
+    let AllDivButtons=document.querySelectorAll('article > button')
+    AllDivP.forEach(paragraph => {paragraph.remove()});
+    AllDivButtons.forEach(button => {button.remove()});
+}
