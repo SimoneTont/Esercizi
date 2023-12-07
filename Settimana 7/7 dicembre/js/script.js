8a7a5880a56d13f77880e40c6a1ddfb6bb77cd53
Simone Tonti <simone.tonti98@gmail.com>
2023-12-07 15:12:22 +0100

    template

--- Settimana 7/7 dicembre/js/script.js
+++ Settimana 7/7 dicembre/js/script.js
@@ -1,1 +1,13 @@
 console.log(window)
+
+document.addEventListener("DOMContentLoaded", () => {
+    let xhr = new XMLHttpRequest ();
+    xhr.open('GET', 'https://striveschool-api.herokuapp.com/books')
+    xhr.send();
+
+    xhr.onreadystatechange = function (){
+        if (xhr.readyState ===4 && xhr.status === 200){
+            let books = JSON.parse(xhr.responseText);
+        console.log(books)}
+        }
+});
